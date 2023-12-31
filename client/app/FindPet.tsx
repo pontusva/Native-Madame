f207544f9ab3032ab75291ac10eee36d7b7aa005
Pontus A <pontus.abrahamsson@iths.se>
2023-12-31 22:49:40 +0100

    Add image to FindPet component

--- client/app/FindPet.tsx
+++ client/app/FindPet.tsx
@@ -1,9 +1,15 @@
-import { View, Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
 
 export default function FindPet() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Find Pet</Text>
+      <Text>Your pets</Text>
+      <Image
+        style={{ width: 200, height: 200, borderRadius: 20 }}
+        source={{
+          uri: 'http://192.168.1.237:8080/static/671b8ee5-8ca3-41a1-b773-65b9edcc533a.jpg',
+        }}
+      />
     </View>
   );
 }
