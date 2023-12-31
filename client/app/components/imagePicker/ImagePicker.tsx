c2ab0dd09c548c5e80d85050c3a8e3876611dd83
Pontus A <pontus.abrahamsson@iths.se>
2023-12-31 18:55:57 +0100

    Add image picker functionality and dependencies

--- client/app/components/imagePicker/ImagePicker.tsx
+++ client/app/components/imagePicker/ImagePicker.tsx
@@ -1,24 +1,80 @@
+import 'react-native-get-random-values';
 import React, { useState, useEffect } from 'react';
 import { Button, Image, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { v4 as uuidv4 } from 'uuid';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+const createFormData = (
+  photo: { fileName: string; type: string; uri: string },
+  body = {}
+) => {
+  const data = new FormData();
+
+  const photoBlob = new Blob([photo.uri], { type: photo.type });
+  data.append('photo', photoBlob, photo.fileName);
+
+  Object.keys(body).forEach(key => {
+    const createFormData = (
+      photo: { fileName: string; type: string; uri: string },
+      body: Record<string, string> = {}
+    ) => {
+      const data = new FormData();
+
+      const photoBlob = new Blob([photo.uri], { type: photo.type });
+      data.append('photo', photoBlob, photo.fileName);
+
+      Object.keys(body).forEach(key => {
+        data.append(key, body[key]);
+      });
+
+      return data;
+    };
+  });
+
+  return data;
+};
 
 export default function ImagePickerMissingPet() {
   const [image, setImage] = useState<string | null>(null);
 
   const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
 
-    console.log(result);
+      delete (result as any).cancelled;
 
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      if (!result.canceled) {
+        setImage(result.assets[0].uri);
+
+        const response = await fetch(result.assets[0].uri);
+        const blob = await response.blob();
+        console.log(result);
+
+        const photo = {
+          fileName: `${uuidv4()}.jpg`,
+          type: 'image/jpeg',
+          uri: result.assets[0].uri,
+        };
+        const formData = createFormData(photo);
+
+        console.log({ formData });
+
+        const uploadResponse = await fetch('https://your-server.com/upload', {
+          method: 'POST',
+          body: formData,
+        });
+        if (!uploadResponse.ok) {
+          throw new Error('Upload failed');
+        }
+      }
+    } catch (error) {
+      console.error('Error picking image:', error);
     }
   };
 
