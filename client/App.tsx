3eb4386f330514164554995379d61c47dc31c8c0
Pontus A <pontus.abrahamsson@iths.se>
2023-12-31 13:07:27 +0100

    Remove SettingsScreen component

--- client/App.tsx
+++ client/App.tsx
@@ -14,7 +14,6 @@ type RootStackParamList = {
   Home: undefined;
   Profile: undefined;
   Details: undefined;
-  SettingsScreen: undefined;
 };
 
 function DetailsScreen() {
@@ -39,20 +38,6 @@ function HomeScreen({
   );
 }
 
-function SettingsScreen({
-  navigation,
-}: NativeStackScreenProps<RootStackParamList>) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
 const HomeStack = createNativeStackNavigator();
 
 function HomeStackScreen() {
@@ -61,20 +46,6 @@ function HomeStackScreen() {
       <HomeStack.Screen name="Home" component={HomeScreen} />
       <HomeStack.Screen name="Details" component={DetailsScreen} />
     </HomeStack.Navigator>
-  );
-}
-
-const SettingsStack = createNativeStackNavigator();
-
-function SettingsStackScreen() {
-  return (
-    <SettingsStack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <SettingsStack.Screen name="SettingsScreen" component={SettingsScreen} />
-      <SettingsStack.Screen name="Details" component={DetailsScreen} />
-    </SettingsStack.Navigator>
   );
 }
 
