a5f9927bcd77b857f32745db69afc2d06b67ebef
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 11:35:49 +0100

    Refactor petProfile function and update PetProfile component

--- backend/src/postgresjs/pet.profile.ts
+++ backend/src/postgresjs/pet.profile.ts
@@ -2,10 +2,11 @@ import sql from './db';
 
 export async function petProfile(petId: string, owner_id: string) {
   const lastSeen = await sql`
-  SELECT * 
+  SELECT pets.*, last_seen.*, images.image_name 
   FROM pets 
   JOIN last_seen ON pets.owner_uid = last_seen.user_uid 
-  WHERE pets.id = ${petId} AND pets.owner_uid = ${owner_id};  
+  LEFT JOIN images ON pets.id = images.pet_id
+  WHERE pets.id = ${petId} AND pets.owner_uid = ${owner_id};
 `;
 
   return lastSeen;
--- client/app/FindPetStackScreen/PetProfile.tsx
+++ client/app/FindPetStackScreen/PetProfile.tsx
@@ -1,5 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
+import * as Location from 'expo-location';
+import { LocationGeocodedAddress } from 'expo-location';
+import { getAuth } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 type StackParamList = {
   'Pet Profile': { petId: string };
@@ -10,14 +14,90 @@ interface PetProfileProps {
   route: PetProfileRouteProp;
 }
 
+interface SpecificPet {
+  specificPet: {
+    id: number;
+    name: string;
+    type: string;
+    description: string;
+    date_lost: string;
+    owner_id: string;
+    created_at: string;
+    pet_id: number;
+    user_uid: string;
+    location: string;
+    seen_at: string;
+    image_name: string;
+  }[];
+}
+
 export default function PetProfile({ route }: PetProfileProps) {
+  const [petProfile, setPetProfile] = useState<SpecificPet | null>(null);
+  const [location, setLocation] = useState<LocationGeocodedAddress[] | null>(
+    null
+  );
   const { petId } = route.params;
+  const userid = getAuth().currentUser?.uid as string;
 
-  console.log(petId);
+  const getPet = async () => {
+    try {
+      const response = await fetch(
+        `http://192.168.1.237:8080/pet-profile/${petId}/${userid}`
+      );
+      const data = await response.json();
+      setPetProfile(data);
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    getPet().then(async data => {
+      const loc = await Location.reverseGeocodeAsync(
+        JSON.parse(data.specificPet[0].location)
+      );
+      setLocation(loc);
+    });
+  }, []);
+
+  console.log(petProfile?.specificPet[0].image_name, location);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Pet Profile</Text>
+      {!!petProfile && !!location && (
+        <>
+          <Text>{petProfile.specificPet[0].name}</Text>
+          <Text>{petProfile.specificPet[0].type}</Text>
+          <Text>{petProfile.specificPet[0].description}</Text>
+          <View>
+            <Text>
+              Försvinnande datum:{' '}
+              {petProfile.specificPet[0].date_lost.split('T')[0]}
+            </Text>
+          </View>
+          <Image
+            style={{ width: 200, height: 200, borderRadius: 20 }}
+            source={{
+              uri: `http://192.168.1.237:8080/static/${petProfile.specificPet[0].image_name}`,
+            }}
+          />
+          {location.map((loc, index) => {
+            return (
+              <View key={index}>
+                <Text>Senast sedd:</Text>
+                <Text>
+                  {loc.name} {loc.street}
+                </Text>
+                <Text>
+                  {loc.city} {loc.postalCode}
+                </Text>
+
+                <Text>{loc.country}</Text>
+              </View>
+            );
+          })}
+        </>
+      )}
     </View>
   );
 }
