f8ad04b0a76c095048879457909a4f2495ed534d
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 19:01:36 +0100

    Refactor SQL queries in pet.ts

--- backend/src/postgresjs/pet.ts
+++ backend/src/postgresjs/pet.ts
@@ -2,11 +2,11 @@ import sql from './db';
 
 export async function getPetImages(userid: string) {
   const images = await sql`
-  SELECT users.name AS user_name, pets.name AS pet_name, pets.id, images.image_name
-  FROM users
-  JOIN pets ON users.uid = pets.owner_uid
-  JOIN images ON pets.id = images.pet_id
-  WHERE users.uid = ${userid}
+    SELECT users.name AS user_name, pets.name AS pet_name, pets.id, images.image_name
+    FROM users
+    JOIN pets ON users.uid = pets.owner_uid
+    JOIN images ON pets.id = images.pet_id
+    WHERE users.uid = ${userid}
   `;
 
   return images;
@@ -18,16 +18,16 @@ export async function lastSeen(
   location: string
 ) {
   const lastSeen = await sql`
-  INSERT INTO last_seen (
-    pet_id,
-    user_uid,
-    location
-  ) VALUES (
-    ${petId},
-    ${userUid},
-    ${location}
-  )
-  RETURNING *
+    INSERT INTO last_seen (
+      pet_id,
+      user_uid,
+      location
+    ) VALUES (
+      ${petId},
+      ${userUid},
+      ${location}
+    )
+    RETURNING *
 `;
 
   return lastSeen;
@@ -35,14 +35,14 @@ export async function lastSeen(
 
 export async function petAlert() {
   const alert = await sql`
-  SELECT p.*, i.image_name
-  FROM pets p
-  JOIN (
-    SELECT owner_uid, MAX(created_at) as latest
-    FROM pets
-    GROUP BY owner_uid
-  ) latest_pets ON p.owner_uid = latest_pets.owner_uid AND p.created_at = latest_pets.latest
-  LEFT JOIN images i ON p.id = i.pet_id;
+    SELECT p.*, i.image_name
+    FROM pets p
+    JOIN (
+      SELECT owner_uid, MAX(created_at) as latest
+      FROM pets
+      GROUP BY owner_uid
+    ) latest_pets ON p.owner_uid = latest_pets.owner_uid AND p.created_at = latest_pets.latest
+    LEFT JOIN images i ON p.id = i.pet_id;
   `;
 
   return alert;
@@ -50,10 +50,11 @@ export async function petAlert() {
 
 export async function community_searchers() {
   const searchers = await sql`
-    SELECT cs.*, u.*, p.*
+    SELECT cs.*, u.*, p.*, i.image_name
     FROM community_searchers cs
     JOIN users u ON cs.user_uid = u.uid
-    JOIN pets p ON cs.pet_id = p.id;
+    JOIN pets p ON cs.pet_id = p.id
+    JOIN images i ON p.id = i.pet_id;
   `;
 
   return searchers;
