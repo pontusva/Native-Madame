bd8573985da4903fc4513c89b4ac54350d506c5f
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 19:06:25 +0100

    Add community images to CommunitySearches component

--- client/app/CommunitySearches.tsx
+++ client/app/CommunitySearches.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+interface Community {
+  cs: {
+    id: number;
+    name: string;
+    description: string;
+    url: string;
+    created_at: string;
+    updated_at: string;
+  }[];
+}
 
 export default function CommunitySearches() {
-  const [community, setCommunity] = useState();
+  const [community, setCommunity] = useState<Community | null>(null);
 
   const getCommunity = async () => {
     const respons = await fetch('http://192.168.1.237:8080/community-searcher');
@@ -21,6 +32,30 @@ export default function CommunitySearches() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Community Searches</Text>
+      {community &&
+        community.cs.map((item: any) => {
+          return (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 20,
+                borderRadius: 20,
+                margin: 20,
+              }}
+              key={item.id}>
+              <Text>{item.name}</Text>
+              <Text>{item.description}</Text>
+              <Image
+                style={{ width: 200, height: 200, borderRadius: 20 }}
+                source={{
+                  uri: `http://192.168.1.237:8080/static/${item.image_name}`,
+                }}
+              />
+            </View>
+          );
+        })}
     </View>
   );
 }
