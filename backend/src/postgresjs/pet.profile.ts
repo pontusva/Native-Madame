9ba2ecfb51684c03963d64ceecd6bbf79cd8d3cf
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 10:48:19 +0100

    Add join with last_seen table in petProfile function

--- backend/src/postgresjs/pet.profile.ts
+++ backend/src/postgresjs/pet.profile.ts
@@ -2,7 +2,10 @@ import sql from './db';
 
 export async function petProfile(petId: string, owner_id: string) {
   const lastSeen = await sql`
-  SELECT * FROM pets WHERE id = ${petId} AND owner_id = ${owner_id}
+  SELECT * 
+  FROM pets 
+  JOIN last_seen ON pets.owner_uid = last_seen.user_uid 
+  WHERE pets.id = ${petId} AND pets.owner_uid = ${owner_id};  
 `;
 
   return lastSeen;
