4bf6b3d3625680d6d3f0f00fee6ca69ad1846156
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 10:29:22 +0100

    Add route prop to PetProfile component

--- client/app/FindPetStackScreen/PetProfile.tsx
+++ client/app/FindPetStackScreen/PetProfile.tsx
@@ -1,6 +1,20 @@
 import { View, Text } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
 
-export default function PetProfile() {
+type StackParamList = {
+  'Pet Profile': { petId: string };
+};
+type PetProfileRouteProp = RouteProp<StackParamList, 'Pet Profile'>;
+
+interface PetProfileProps {
+  route: PetProfileRouteProp;
+}
+
+export default function PetProfile({ route }: PetProfileProps) {
+  const { petId } = route.params;
+
+  console.log(petId);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Pet Profile</Text>
