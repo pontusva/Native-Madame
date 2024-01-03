2d904f18d162a26709e140a5ab2504a438b15b09
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 10:05:14 +0100

    Remove unused import in PetAlert.tsx

--- client/app/PetAlert.tsx
+++ client/app/PetAlert.tsx
@@ -1,5 +1,4 @@
 import { Text, View } from 'react-native';
-import { useRoute } from '@react-navigation/native';
 export default function PetAlert() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
