38b843ff87183dea8e4cab921ad2f59ea8c5b0c5
Pontus A <pontus.abrahamsson@iths.se>
2024-01-03 18:58:18 +0100

    Add community search functionality

--- client/app/CommunitySearches.tsx
+++ client/app/CommunitySearches.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 
 export default function CommunitySearches() {
+  const [community, setCommunity] = useState();
+
+  const getCommunity = async () => {
+    const respons = await fetch('http://192.168.1.237:8080/community-searcher');
+
+    const data = await respons.json();
+
+    setCommunity(data);
+  };
+
+  useEffect(() => {
+    getCommunity();
+  }, []);
+
+  console.log(community);
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Community Searches</Text>
