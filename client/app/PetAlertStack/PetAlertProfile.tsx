6c1604773bf99d63f3dbef54bf531ac43604c84c
Pontus A <pontus.abrahamsson@iths.se>
2024-01-04 10:00:46 +0100

    Add route prop to PetAlertProfile component

--- client/app/PetAlertStack/PetAlertProfile.tsx
+++ client/app/PetAlertStack/PetAlertProfile.tsx
@@ -1,6 +1,21 @@
 import { View, Text, SafeAreaView } from 'react-native';
+import { RouteProp } from '@react-navigation/native';
 
-export default function PetAlertProfile() {
+type StackParamList = {
+  'Alert Profile': { petId: string };
+};
+type PetProfileRouteProp = RouteProp<StackParamList, 'Alert Profile'>;
+
+interface PetProfileProps {
+  route?: PetProfileRouteProp;
+}
+
+export default function PetAlertProfile({ route }: PetProfileProps) {
+  if (!route) return null;
+  const { petId } = route.params;
+
+  console.log(petId);
+
   return (
     <SafeAreaView>
       <View>
